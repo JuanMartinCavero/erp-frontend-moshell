65586806bb091631d005df8c6c12fdec276912a1
27adrian <munozroncaldavidadrian@gmail.com>
2026-03-14 12:38:55 -0500

    Correccion del app.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,27 @@
-// src/App.jsx
-import React from 'react';
-import DashboardPage from './features/dashboard/DashboardPage';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+
+import Login from "./pages/Login";
+import DashboardPage from "./features/dashboard/DashboardPage";
+import RolesPage from "./features/roles/RolesPage";
 
 function App() {
-  return <DashboardPage />;
+  return (
+      <Routes>
+
+        {/* Login sin layout */}
+        <Route path="/" element={<Login />} />
+
+        {/* Rutas con Sidebar y Header */}
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/admin/roles" element={<RolesPage />} />
+        </Route>
+
+      </Routes>
+  );
 }
 
 export default App;
