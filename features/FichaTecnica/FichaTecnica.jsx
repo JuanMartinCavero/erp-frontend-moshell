36329b76170a65ac69fb5b2f86807bbce879f4dc
Juan Cavero <jhony2412@yahoo.com>
2026-07-25 11:15:16 -0500

    dinamizmo de tabs en Ficha Tecnica resumen de Especificaciones de Tallas y Color

--- features/FichaTecnica/FichaTecnica.jsx
+++ features/FichaTecnica/FichaTecnica.jsx
@@ -215,6 +215,7 @@ export default function FichaTecnica() {
               isEditing={isEditing}
               onUpdate={updateSpecs}
               onUpdateMachine={updateMachine}
+              pedido={pedido} 
             />
           )}
 
@@ -230,7 +231,7 @@ export default function FichaTecnica() {
             />
           )}
 
-          {activeTab === 3 && <SizeSpecsTab sizeSpecs={techSheet.size_specs} />}
+         {activeTab === 3 && <SizeSpecsTab sizeSpecs={techSheet.size_specs} pedido={pedido} />} 
 
           <ReferenceImages images={techSheet.images} />
         </div>
