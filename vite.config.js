1e1cf5572a71fc473b751f703a35643b2055000f
Juan Cavero <jhony2412@yahoo.com>
2026-07-31 13:21:42 -0500

    prueba

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
-    },
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 100,
+      },
+      host: true,
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        }
       }
     }
   }
