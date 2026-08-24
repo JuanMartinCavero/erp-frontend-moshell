cbfd6e835394e75ad3430b277fb7ea8c0e0a2c33
Juan Cavero <jhony2412@yahoo.com>
2026-08-24 18:00:59 -0500

    cambio minusculas

--- features/inventory/components/tabEntradaCompra.jsx
+++ features/inventory/components/tabEntradaCompra.jsx
@@ -3,7 +3,7 @@ import { registrarMovimiento } from "../../../services/kardexApi";
 import { buscarMaterialPorCodigo } from "../../../services/materialApi";
 import axiosClient from "../../../services/axiosClient";
 
-export function TabEntradaCompra({ onClose, onRefresh }) {
+export function tabEntradaCompra({ onClose, onRefresh }) {
   const [material, setMaterial] = useState(null);
   const [materialId, setMaterialId] = useState(null);
   const [codigo, setCodigo] = useState("");
